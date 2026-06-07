9938ac76f34c97e6cf53c92635dfcf1298aec027
ShuWald <shubhambirendarwaldiya12345@gmail.com>
2026-06-07 14:52:13 -0700

    Implemented Scaler and fixed data visualization

--- dataevaluator.py
+++ dataevaluator.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import mnist_reader
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from seed import seed
 
 class DataEvaluator:
@@ -22,23 +23,24 @@ class DataEvaluator:
         }
         self.X_temp, self.y_temp = mnist_reader.load_mnist(dataset_path, kind='train')
         self.X_test, self.y_test = mnist_reader.load_mnist(dataset_path, kind='t10k')
-        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_temp, self.y_temp, test_size=0.15, random_state=seed)
 
-    def get_data(self):
+    def get_data(self): 
+        self.scaler = StandardScaler()
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(self.X_temp, self.y_temp, test_size=0.15, random_state=seed)
+        self.X_train = self.scaler.fit_transform(self.X_train)
+        self.X_val = self.scaler.transform(self.X_val)
+        self.X_test = self.scaler.transform(self.X_test)
         return self.X_train, self.y_train, self.X_val, self.y_val, self.X_test, self.y_test
 
     def plot_clothing_distribution(self, save_path='figures/clothing_distribution.png', show=True):
-        df_ytrain = pd.DataFrame(self.y_train, columns=['label'])
-        df_yval = pd.DataFrame(self.y_val, columns=['label'])
-        df_ytest = pd.DataFrame(self.y_test, columns=['label'])
-        train_labels = df_ytrain['label'].value_counts().sort_index()
-        val_labels = df_yval['label'].value_counts().sort_index()
-        test_labels = df_ytest['label'].value_counts().sort_index()
+        df_train, df_val, df_test = [pd.DataFrame(y, columns=['label']) for y in (self.y_train, self.y_val, self.y_test)]
+        train_labels, val_labels, test_labels = [df['label'].value_counts().sort_index() for df in (df_train, df_val, df_test)]
+        labels = [self.clothing_types[i] for i in train_labels.index]
 
         plt.figure(figsize=(10, 5))
-        plt.bar([self.clothing_types[i] for i in train_labels.index], train_labels.values, alpha=0.65, label='Train')
-        plt.bar([self.clothing_types[i] for i in val_labels.index], val_labels.values, alpha=0.65, label='Validation')
-        plt.bar([self.clothing_types[i] for i in test_labels.index], test_labels.values, alpha=0.65, label='Test')
+        plt.bar(labels, train_labels.values, alpha=0.65, label='Train')
+        plt.bar(labels, val_labels.values, bottom=train_labels.values, alpha=0.65, label='Validation')
+        plt.bar(labels, test_labels.values, bottom=train_labels.values + val_labels.values, alpha=0.65, label='Test')
         plt.xlabel('Clothing')
         plt.ylabel('Count')
         plt.title('Distribution of Clothing Types in Train, Validation, and Test Sets')
--- todo.md
+++ todo.md
@@ -1,6 +1,5 @@
 \- Cross-validation(K-Fold?)?
 \- Explore models/tuning
-    \- Scaling
     \- Iterations or scaling for Logistic
     ~~\- Bagging? possible using `BaggingClassifier` (ensembles are impractical for larger models like MLP)~~
     \- Hyperparameters
